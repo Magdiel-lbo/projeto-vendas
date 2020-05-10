97d4147c723f26a99be0ffb850d9fc6bc1a743c2
santosmagdiel931@gmail.com <santosmagdiel931@gmail.com>
2020-05-10 19:06:05 -0300

    projeto criado

---
+++ projetovendas/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using projetovendas.Models;
+
+namespace projetovendas.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpPost]
+        [HttpGet]
+        public IActionResult Index(HomeModel formulaeio)
+        {
+            CarregarListaDados();
+            return View();
+        }
+        public void CarregarListaDados()
+        {
+            HomeModel objHomeModel = new HomeModel();
+            ViewBag.lista = objHomeModel.GetALL();
+        }
+    }
+}
---
+++ projetovendas/Models/HomeModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace projetovendas.Models
+{
+    public class HomeModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public List<HomeModel> GetALL()
+        {
+            HomeModel item;
+            List<HomeModel> lista = new List<HomeModel>();
+
+            item = new HomeModel();
+            item.Id = 1;
+            item.Name = "Magdiel";
+            lista.Add(item);
+
+            item = new HomeModel();
+            item.Id = 2;
+            item.Name = "aluno";
+            lista.Add(item);
+
+            return lista;
+        }
+    }
+}
